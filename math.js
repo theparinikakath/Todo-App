da17f26be4dff74fc21ec8917d06a23ea53ee2c8
Parinika Kath <121677768+theparinikakath@users.noreply.github.com>
2024-07-27 21:42:52 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,13 @@
+export const sum=(a,b)=>a+b;
+export const mul=(a,b)=>a*b;
+export const g=9.8;
+export const pi=3.14;
+
+// let obj={
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+//     pi:pi,
+// };
+// module.exports=obj;
+// module.exports=123;
---
+++ script.js
@@ -0,0 +1,24 @@
+// // for(let i=0;i<5;i++){
+// //     console.log("hi",i);
+// // }
+// // console.log("bye");
+// console.log(process.argv);
+
+// let args=process.argv;
+// for(let i=2;i<args.length;i++){
+//     console.log("hello and welcome to",args[i]);
+// }
+
+// const someval=require("./fruits");
+// console.log(someval[0].name);
+
+// console.log(math.sum(2,2));
+// console.log(math.g);
+
+// import{sum,pi} from "./math.js";
+// console.log(sum(1,2));
+
+import { generate} from "random-words";
+
+console.log(generate());
+//output: 'army'
